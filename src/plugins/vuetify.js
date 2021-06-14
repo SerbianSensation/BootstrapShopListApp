1ee8bcbc335252dd73cfb12466d7d93a6dacdbc9
Stefan Gligorevic <stefangligorevic@gmail.com>
2021-06-14 16:47:17 -0400

    Removed the plugins directory since Vuetify is no longer needed

--- src/plugins/vuetify.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
-Vue.use(Vuetify);
-
-//exporting an instance of vuetify
-export default new Vuetify();
