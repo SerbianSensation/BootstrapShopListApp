b158714d8843d29cbdb9c959c93511e39236c2e6
Stefan Gligorevic <stefangligorevic@gmail.com>
2021-06-15 14:49:05 -0400

    Edited CartItem component to make it look a little neater.

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -3,18 +3,21 @@
 
 <template>
   <div class="list-group-item text-left">
-    <h3 style="display: inline-block">{{ item.name }}</h3>
-    <span class="pad-left">Complete: {{ item.complete }} Quantity: {{ item.order }}</span>
-    <div class="pull-right">
-      <b-icon-plus-circle class="icon" @click="$emit('incOrder', item.id)"></b-icon-plus-circle>
-      <b-icon-patch-minus class="icon" @click="$emit('decOrder', item.id)"></b-icon-patch-minus>
-      <b-icon-x-circle-fill v-if="item.complete" class="icon" @click="$emit('updateComplete', item.id)"></b-icon-x-circle-fill>
-      <b-icon-check-circle v-else class="icon" @click="$emit('updateComplete', item.id)"></b-icon-check-circle>
-      <router-link :to="{ name: 'editForm', params: { id: item.id } }">
-        <b-icon-pencil-fill class="icon"></b-icon-pencil-fill>
-      </router-link>
-      <b-icon-trash-fill class="icon" @click="$emit('deleteItem', item);"></b-icon-trash-fill>
-    </div>
+      <h3 class="inline-block">{{ item.name }}</h3>
+      <span v-if="item.complete" class="pad-left complete inline">Complete</span>
+      <span v-else class="pad-left incomplete inline">Incomplete</span>
+      <span class="pad-left">Quantity: {{ item.order }}</span>
+      <!-- icons off to the right !-->
+      <div class="pull-right">
+        <b-icon-plus-circle class="icon" @click="$emit('incOrder', item.id)"></b-icon-plus-circle>
+        <b-icon-patch-minus class="icon" @click="$emit('decOrder', item.id)"></b-icon-patch-minus>
+        <b-icon-x-circle-fill v-if="item.complete" class="icon" @click="$emit('updateComplete', item.id)"></b-icon-x-circle-fill>
+        <b-icon-check-circle v-else class="icon" @click="$emit('updateComplete', item.id)"></b-icon-check-circle>
+        <router-link :to="{ name: 'editForm', params: { id: item.id } }">
+          <b-icon-pencil-fill class="icon"></b-icon-pencil-fill>
+        </router-link>
+        <b-icon-trash-fill class="icon" @click="$emit('deleteItem', item);"></b-icon-trash-fill>
+      </div>
   </div>
 </template>
 
@@ -27,3 +30,18 @@ export default {
   }
 };
 </script>
+
+<style>
+.complete {
+  color: green;
+}
+.incomplete {
+  color: red;
+}
+.inline {
+  display: inline;
+}
+.inline-block {
+  display: inline-block;
+}
+</style>
